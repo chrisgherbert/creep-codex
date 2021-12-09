31817fe2e2a5cae6347b125c7d528095e4cf2a26
chrisgherbert <chris@chrisgherbert.com>
2021-12-08 20:47:56 -0500

    Added options for fs_method in .env

--- config/application.php
+++ config/application.php
@@ -74,6 +74,7 @@ define('NONCE_SALT', env('NONCE_SALT'));
 // define('AUTOMATIC_UPDATER_DISABLED', true);
 // define('DISALLOW_FILE_EDIT', true);
 define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
+define('FS_METHOD', env('FS_METHOD'));
 
 /**
  * Bootstrap WordPress
